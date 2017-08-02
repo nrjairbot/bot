d87a29e3eea3ec49b7d257c63e436924f62d1912
Raphael <raphaelbezd12@gmail.com>
2017-08-02 15:18:49 +0200

    semicolon

--- energy game/popup.js
+++ energy game/popup.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
 newNumber = () => {
     const number = document.getElementById("number").value;
     chrome.storage.sync.set({'number': number}, function() {
-      document.getElementById("number").value = null
-      document.getElementById("message").innerText = "Saved, Please refresh the game page"
+      document.getElementById("number").value = null;
+      document.getElementById("message").innerText = "Saved, Please refresh the game page";
     });
 }
